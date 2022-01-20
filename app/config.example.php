e8b95e43e6904e4437a013f8cac411661da0548e
Gustavo Novaro <gnovaro@users.noreply.github.com>
2022-01-20 11:22:28 +0100

    Update rss default feed page url for la vanguardia

--- app/config.example.php
+++ app/config.example.php
@@ -3,12 +3,12 @@
 
     // For Temperature
     $OPEN_WEATHER_API_KEY = '';
-    $CITY = 'Castelldefels';
+    $CITY = 'Barcelona';
     $STATE = 'Barcelona';
     $COUNTRY = 'es';
     $UNITS = 'metric';
 
     $OPEN_WEATHER_API_CALL_URL = "https://api.openweathermap.org/data/2.5/weather?q=$CITY,$STATE,$COUNTRY&appid=$OPEN_WEATHER_API_KEY&units=$UNITS";
-    $RSS_URL = 'https://www.lavanguardia.com/feed/rss/home';
+    $RSS_URL = 'https://www.lavanguardia.com/rss/home.xml';
     $RSS2JSON_API_KEY = '';
     $RSS_FEED_LIMIT = 3;
